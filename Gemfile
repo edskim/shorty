4f8de1200603e58a17e997619314f020fc84dad1
alexis finch <agent.fin@gmail.com>
2012-07-31 15:38:17 -0700

    Initial commit with update to .gitignore and Gemfile

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.7'
+
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.4'
+  gem 'coffee-rails', '~> 3.2.2'
+  gem 'uglifier', '>= 1.2.3'
+end
+
+group :production do
+	gem 'pg', '0.12.2'
+end
+
+gem 'jquery-rails', '2.0.0'
+
+# To use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+# gem 'jbuilder'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shorty::Application.config.secret_token = '4fc279f62371cccd0454019c77e968a75019f42aff27c1ccfc89b6aa10072e16cd8729bb559a101c8af259a5e1c7452a7bdeb3b6210c8955a87e7c0076fe2869'
